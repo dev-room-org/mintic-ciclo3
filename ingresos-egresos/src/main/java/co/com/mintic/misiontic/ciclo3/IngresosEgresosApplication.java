b0747c492b4fc07678d7d0274daeecf7f4ea4577
DEVROOM\AlexanderDiazGarcia12 <alexander.diazg@outlook.com>
2022-08-12 11:48:50 -0500

    Primer commit que cotiene la creacion del proyecto

---
+++ ingresos-egresos/src/main/java/co/com/mintic/misiontic/ciclo3/IngresosEgresosApplication.java
@@ -0,0 +1,13 @@
+package co.com.mintic.misiontic.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngresosEgresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngresosEgresosApplication.class, args);
+	}
+
+}
---
+++ ingresos-egresos/src/test/java/co/com/mintic/misiontic/ciclo3/IngresosEgresosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mintic.misiontic.ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngresosEgresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
