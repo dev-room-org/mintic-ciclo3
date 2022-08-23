f22656b26e68497dc7262381b17f68d184261472
Marcela Aristizabal <marcea@Marcelas-MacBook-Pro.local>
2022-08-23 18:23:10 -0500

    Código modificado

--- ingresos-egresos/src/main/java/co/com/mintic/misiontic/ciclo3/IngresosEgresosApplication.java
+++ ingresos-egresos/src/main/java/co/com/mintic/misiontic/ciclo3/IngresosEgresosApplication.java
@@ -1,26 +1,13 @@
 package co.com.mintic.misiontic.ciclo3;
 
-import co.com.mintic.misiontic.ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
 public class IngresosEgresosApplication {
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hola Ciclo 3... Saldremos vivos de esto!";
-	}
-
-	@GetMapping("/test")
-	public String test(){
-		Empresa emp = new Empresa("SOLAR SAS", "Calle la geta", "3213213211","800212132-3");
-		emp.setNombre("SOLAR LTDA");
-		return emp.getNombre();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IngresosEgresosApplication.class, args);
--- ingresos-egresos/src/main/java/co/com/mintic/misiontic/ciclo3/controller/EmpresaController.java
+++ ingresos-egresos/src/main/java/co/com/mintic/misiontic/ciclo3/controller/EmpresaController.java
@@ -6,16 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
-public class ControllerFull {
+public class EmpresaController {
     @Autowired
     EmpresaService empresaService;
 
-    @GetMapping ({"/","/VerEmpresas"})
+    @GetMapping ({"/VerEmpresas"})
     public String viewEmpresas(Model model){
         List<Empresa> listaEmpresas=empresaService.getAllEmpresas();
         model.addAttribute("emplist",listaEmpresas);
